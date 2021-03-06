9d964176b1642689f66b48e8b5eb1b744b0ffb36
Michael Henderson <mdhender@mdhender.com>
2021-03-06 10:58:54 -0700

    added feature to validate input dates

--- stdlib/stdlib.go
+++ stdlib/stdlib.go
@@ -29,30 +29,97 @@ import (
 	"strings"
 )
 
-// Date translates QIF date to a string with the date formatted as yyyy/mm/dd
-// The QIF date is formatted as mm/dd'yy. The month can be one or two digits
+// Date translates QIF date to a string with the date formatted as yyyy/mm/dd.
+// The QIF date is a string which looks like
+//    digit digit? slash (space | digit) digit tic digit digit
+// It is is formatted as mm/dd'yy. The month can be one or two digits
 // (eg, January is `1` while October is `10`). The day must be two characters,
 // but the first character may be a space instead of a zero. For example,
 // `01` and ` 1` are both the first day of the month. The year must be two
 // digits, and we're assuming it is always in the 21st century (eg, `16` is
 // converted to 2016, not 1916).
 func Date(b []byte) string {
-	// 9/ 3'16 -> 2016/09/03
-	// 9/13'16 -> 2016/09/13
-	if len(b) == 7 && b[1] == '/' && b[4] == '\'' {
-		mm, dd, yy := b[0:1], b[2:4], b[5:]
-		return fmt.Sprintf("%4d/%02d/%02d", ToInt(yy)+2000, ToInt(mm), ToInt(dd))
+	atoi := func(b []byte) int {
+		return int(b[0] - '0')
+	}
+	isdigit := func(b []byte) bool {
+		return len(b) != 0 && '0' <= b[0] && b[0] <= '9'
 	}
 
-	// 12/ 9'16 -> 2016/12/09
-	// 12/19'16 -> 2016/12/19
-	if len(b) == 8 && b[2] == '/' && b[5] == '\'' {
-		mm, dd, yy := b[0:2], b[3:6], b[6:]
-		return fmt.Sprintf("%4d/%02d/%02d", ToInt(yy)+2000, ToInt(mm), ToInt(dd))
+	var cc, yy, mm, dd int
+
+	if isdigit(b) { // digit
+		b, mm = b[1:], atoi(b)
+	} else {
+		return "****/**/**"
 	}
 
-	// invalid date
-	return "****/**/**"
+	if isdigit(b) { // digit?
+		b, mm = b[1:], mm*10+atoi(b)
+	}
+
+	if len(b) > 0 && b[0] == '/' { // slash
+		b = b[1:]
+	} else {
+		return "****/**/**"
+	}
+
+	// space | digit
+	if len(b) > 0 && b[0] == ' ' { // space
+		b = b[1:]
+	} else if isdigit(b) { // digit
+		b, dd = b[1:], atoi(b)
+	} else {
+		return "****/**/**"
+	}
+
+	if isdigit(b) { // digit
+		b, dd = b[1:], dd*10+atoi(b)
+	} else {
+		return "****/**/**"
+	}
+
+	if len(b) > 0 && b[0] == '\'' { // tic
+		b, cc = b[1:], 2000
+	} else {
+		return "****/**/**"
+	}
+
+	if isdigit(b) { // digit
+		b, yy = b[1:], atoi(b)
+	} else {
+		return "****/**/**"
+	}
+
+	if isdigit(b) { // digit
+		b, yy = b[1:], yy*10+atoi(b)
+	} else {
+		return "****/**/**"
+	}
+
+	yyyy := cc + yy
+	switch mm {
+	case 1, 3, 5, 7, 8, 10, 12: // 31 days
+		if !(1 <= dd && dd <= 31) {
+			return "****/**/**"
+		}
+	case 4, 6, 9, 11: // 30 days
+		if !(1 <= dd && dd <= 30) {
+			return "****/**/**"
+		}
+	case 2: // february
+		if yyyy%4 == 0 && yyyy != 2000 { // leap year
+			if !(1 <= dd && dd <= 29) {
+				return "****/**/**"
+			}
+		} else {
+			if !(1 <= dd && dd <= 28) {
+				return "****/**/**"
+			}
+		}
+	}
+
+	return fmt.Sprintf("%4d/%02d/%02d", yyyy, mm, dd)
 }
 
 // Dup returns an exact copy of a slice.
---
+++ stdlib/stdlib_test.go
@@ -0,0 +1,114 @@
+/*
+ * qif - a package to convert QIF data
+ *
+ * Copyright (c) 2021 Michael D Henderson
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package stdlib_test
+
+import (
+	"github.com/mdhender/qif/stdlib"
+	"testing"
+)
+
+func TestDate(t *testing.T) {
+	// Specification: Date
+
+	// When "9/ 3'16" is converted
+	// Then it has the value "2016/09/03"
+	input, expected := "9/ 3'16", "2016/09/03"
+	yields := stdlib.Date([]byte(input))
+	if expected != yields {
+		t.Errorf("input of %q yields %q: expected value is %q\n", input, yields, expected)
+	}
+
+	// When "9/ 3'16" is converted
+	// Then it has the value "2016/09/13"
+	input, expected = "9/13'16", "2016/09/13"
+	yields = stdlib.Date([]byte(input))
+	if expected != yields {
+		t.Errorf("input of %q yields %q: expected value is %q\n", input, yields, expected)
+	}
+
+	// When "12/ 9'16" is converted
+	// Then it has the value "2016/12/09"
+	input, expected = "12/ 9'16", "2016/12/09"
+	yields = stdlib.Date([]byte(input))
+	if expected != yields {
+		t.Errorf("input of %q yields %q: expected value is %q\n", input, yields, expected)
+	}
+
+	// When "12/19'16" is converted
+	// Then it has the value "2016/12/19"
+	input, expected = "12/19'16", "2016/12/19"
+	yields = stdlib.Date([]byte(input))
+	if expected != yields {
+		t.Errorf("input of %q yields %q: expected value is %q\n", input, yields, expected)
+	}
+
+	// When "ab/cd'ee" is converted
+	// Then it has the value "****/**/**"
+	input, expected = "ab/cd'ee", "****/**/**"
+	yields = stdlib.Date([]byte(input))
+	if expected != yields {
+		t.Errorf("input of %q yields %q: expected value is %q\n", input, yields, expected)
+	}
+
+	// When "1/32'00" is converted
+	// Then it has the value "****/**/**"
+	input, expected = "1/32'00", "****/**/**"
+	yields = stdlib.Date([]byte(input))
+	if expected != yields {
+		t.Errorf("input of %q yields %q: expected value is %q\n", input, yields, expected)
+	}
+
+	// When "2/29'00" is converted
+	// Then it has the value "****/**/**"
+	input, expected = "2/29'00", "****/**/**"
+	yields = stdlib.Date([]byte(input))
+	if expected != yields {
+		t.Errorf("input of %q yields %q: expected value is %q\n", input, yields, expected)
+	}
+
+	// When "2/29'01" is converted
+	// Then it has the value "****/**/**"
+	input, expected = "2/29'01", "****/**/**"
+	yields = stdlib.Date([]byte(input))
+	if expected != yields {
+		t.Errorf("input of %q yields %q: expected value is %q\n", input, yields, expected)
+	}
+
+	// When "2/29'04" is converted
+	// Then it has the value "2004/02/29"
+	input, expected = "2/29'04", "2004/02/29"
+	yields = stdlib.Date([]byte(input))
+	if expected != yields {
+		t.Errorf("input of %q yields %q: expected value is %q\n", input, yields, expected)
+	}
+
+	// When "11/31'00" is converted
+	// Then it has the value "****/**/**"
+	input, expected = "11/31'00", "****/**/**"
+	yields = stdlib.Date([]byte(input))
+	if expected != yields {
+		t.Errorf("input of %q yields %q: expected value is %q\n", input, yields, expected)
+	}
+}
